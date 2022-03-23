b58a04c75c9ae88a2f43d3361c5141570bc9b66d
jarod <jarod@durkin.app>
2022-03-22 23:39:35 -0400

    quick setup for dev env

---
+++ dev.bash
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export DISCORD_TOKEN="$(cat CRED_TOKEN)"
+export DISCORD_CLIENT_ID="$(cat CRED_CLIENTID)"
+export DISCORD_DEV_SERVER="$(cat CRED_DEVSERVER)"
+export COOKIE_VALUE="$(cat CRED_ESPN)"
