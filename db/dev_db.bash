2519d9f7d249be95850447e2ea584deabccaf761
jaroddurkin <jarod@durkin.app>
2022-06-27 18:55:55 -0400

    remove open dev database command

--- db/dev_db.bash
+++
@@ -1,1 +0,0 @@
-docker run -d --name postgres-fb -p 5432:5432 -e POSTGRES_USER=root -e POSTGRES_DB=fantasybot -e POSTGRES_PASSWORD=root -v /Users/jarod/Repos/tmp/postgres:/var/lib/postgresql/data postgres
